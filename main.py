4e7310b6e298d28a74a3245d6bca8d0f3a8f5f4a
EasterlyIrk <riker.wachtler@gmail.com>
2017-02-22 12:21:20 -0800

    add some comments n stuff

--- main.py
+++ main.py
@@ -5,25 +5,30 @@ import ast
 import sys
 
 
+		
 def handle_token(type, token, (srow, scol), (erow, ecol), line):
+	# Return the info about the tokens, if it's a NAME token then replace it
+
 	if tokenize.tok_name[type] == "NAME":
 		token = token_names.get(token, token)
 	return (type, token, (srow, scol), (erow, ecol), line)
 
 
-def run(assignments="assignments.txt",open_from="peoples.txt",to_exec=True):
+def run(assignments="assignments.txt",open_from="peoples.txt"):
 	with open(assignments, "r") as f:
+		# Read the replacements into token_names
 		global token_names
 		token_names = ast.literal_eval(f.read())
 
-
 	with open(open_from) as source:
+		# Get the tokenized version of the input, replace it, and untokenize into pretty output
 		tokens = tokenize.generate_tokens(source.readline)
 		handled_tokens = (handle_token(*token) for token in tokens)
 
 		output = tokenize.untokenize(handled_tokens)
 
-	with open(open_from[:-3]+"-output.txt",'w') as outfile:
+	with open(open_from[:-4]+"-output.txt",'w') as outfile:
+		# Write to the output file
 		outfile.write(output)
 	
 	return output
@@ -40,4 +45,3 @@ if __name__ == "__main__":
 	else:
 		try:run()
 		except:pass
-
